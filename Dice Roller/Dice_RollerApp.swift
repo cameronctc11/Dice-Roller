fbdf9590b6866dcb677823e40d531af76d3f1298
cameronmckenzie <cameronctc11@gmail.com>
2025-11-04 16:34:02 -0700

    Initial Commit

---
+++ Dice Roller/Dice_RollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_RollerApp.swift
+//  Dice Roller
+//
+//  Created by McKenzie, Cameron - Student on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Dice_RollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dice RollerTests/Dice_RollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_RollerTests.swift
+//  Dice RollerTests
+//
+//  Created by McKenzie, Cameron - Student on 11/4/25.
+//
+
+import Testing
+@testable import Dice_Roller
+
+struct Dice_RollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
