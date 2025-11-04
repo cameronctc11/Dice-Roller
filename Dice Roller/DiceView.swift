f501b4df32880e1adbffb521c8500395b8aadf14
cameronmckenzie <cameronctc11@gmail.com>
2025-11-04 16:36:21 -0700

    Finished Dice Roller Game

---
+++ Dice Roller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  Dice Roller
+//
+//  Created by McKenzie, Cameron - Student on 11/4/25.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
